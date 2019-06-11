a66b886da292f7acbfda552185b80c0d0e3b0713
Abhishek Guleri <43719098+0ri0n24@users.noreply.github.com>
2019-06-11 18:30:37 +0530

    clean the comment
    Remove the unwanted lines in the comment. Make it easily readable.

--- C++/Arrays/Minimize the Cell Wastage.cpp
+++ C++/Arrays/Minimize the Cell Wastage.cpp
@@ -14,44 +14,22 @@ Now, it is important for every company to have a banner.
 
 Since companies want to save their resources, they decided to convert their motto into a banner.
 
-How this works is, you create a square matrix of size N x N, and fill in the elements of this matrix using the motto in row-major order.
+How this works is, you create a square matrix of size N x N, and fill in the elements of this
+matrix using the motto in row-major order.The remaining spaces in the matrix are called waste
+cells and are to be represented as '?'.You have to choose N in such a way that the number of
+waste cells is minimized.
 
-The remaining spaces in the matrix are called waste cells and are to be represented as '?'.
-
-You have to choose N in such a way that the number of waste cells is minimized.
-Input
-
+Input:
 Input consists of one line only, containing the company motto
-Output
 
+Output:
 Output must consist of the matrix formed from the input string
-Notes
 
+Notes:
 The length of the input string will not exceed 1000
-
 Sample Input 0
-
 nike - just do it!
-
 Sample Output 0
-
-nike 
-- jus
-t do 
-it!??
-?????
-
-Sample Input 1
-
-oneplus - never settle!
-
-Sample Output 1
-
-onepl
-us - 
-never
- sett
-le!??
 
 */
 
