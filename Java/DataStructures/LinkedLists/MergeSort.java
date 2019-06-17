335dd369daba82a60e1059a7edd520d7ea02fd30
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-06-17 22:03:09 +0530

    Pushed Java SLL inbuilt functions

---
+++ Java/DataStructures/LinkedLists/MergeSort.java
@@ -0,0 +1,45 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class MergeSort {
+    public static void main(String[] args){
+        //List A
+        LinkedList<Integer> linkedList=new LinkedList<>();
+        Scanner scanner=new Scanner(System.in);
+        int na,data,temp;
+        System.out.println("Enter number of nodes of list A -");
+        na=scanner.nextInt();
+        System.out.println("Enter the values -");
+        while(na!=0){
+            data=scanner.nextInt();
+            linkedList.addLast(data);
+            na--;
+        }
+        //List B
+        LinkedList<Integer> linkedList1 =new LinkedList<>();
+        System.out.println("Enter number of nodes of list B -");
+        int nb=scanner.nextInt();
+        System.out.println("Enter the values ");
+        while (nb!=0){
+            data=scanner.nextInt();
+            linkedList1.addLast(data);
+            nb--;
+        }
+        //Now appending A and B
+        linkedList1.addAll(linkedList);
+        System.out.println("Before sorting - Merged list -");
+        System.out.println(linkedList1);
+        //Sorting the appended list
+        for(int i=0;i<linkedList1.size();i++){
+            for(int j=i+1;j<linkedList1.size();j++) {
+                if (linkedList1.get(i) > linkedList1.get(j)) {
+                    temp=linkedList1.get(i);
+                    linkedList1.set(i,linkedList1.get(j));
+                    linkedList1.set(j,temp);
+                }
+            }
+        }
+        System.out.println("After Sorting -");
+        System.out.println(linkedList1);
+    }
+}
