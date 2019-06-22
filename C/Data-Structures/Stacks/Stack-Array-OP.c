6d4d8fa4ac63997d2fe48112a29c637b46b64565
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-06-22 16:46:53 +0530

    Stacks with arrays

--- C/Data-Structures/Stacks/Stack-Array-OP.c
+++ C/Data-Structures/Stacks/Stack-Array-OP.c
@@ -11,12 +11,12 @@ int stack[100],i,j,ch,n,top=-1;
 void push();
 void pop();
 void printStack();
-void peek();
+int peek();
 int main(){
+      printf("Enter the number of elements you want to enter in the stack:\n");
+    scanf("%d",&n);
     while(1){
-        printf("Enter the number of elements you want to enter in the stack:\n");
-        scanf("%d",&n);
-        printf("1.Push\n2.Pop\n3.Print Stack\n4.Exit\n");
+        printf("1.Push\n2.Pop\n3.Print Stack\n4.Peek\n5.Exit\n");
         printf("Enter your choice :\n");
         scanf("%d",&ch);
         switch (ch)
@@ -27,7 +27,7 @@ int main(){
             break;
         case 3: printStack();
             break;
-        case 4: peek();
+        case 4: if(peek()==0){printf("Underflow\n");}else{printf("%d\n",peek());};
             break;
         case 5: ; exit(0);
             break;
@@ -38,17 +38,38 @@ int main(){
 }
 
 void push(){
-
+    int data;
+    if(top==n){
+        printf("Stack Overflow\n");
+    }else{
+        printf("Enter the data:\n");
+        scanf("%d",&data);
+        top=top+1;
+        stack[top]=data;
+    }
 }
 
 void pop(){
-
+    if(top==-1){
+        printf("Stack Underflow\n");
+    }else{
+        top=top-1;
+    }
 }
 
 void printStack(){
-
+    if(top==-1){
+        printf("Stack is empty\n");
+    }
+    for(i=top;i>=0;i--){
+        printf("%d\n",stack[i]);
+    }
 }
 
-void peek(){
-
+int peek(){
+    if(top==-1){
+        return 0;
+    }else{
+        return stack[top];
+    }
 }
