fbec2313652401ce20b81c2bbae3fcddab491731
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-08-18 22:38:07 +0530

    Pushed code fixes for Digital 2020

--- TNQT-2020/One.java
+++ TNQT-2020/One.java
@@ -5,26 +5,41 @@
  * -----
  * Copyright (c) 2019 Gurubalan Harikrishnan
  */
-
+// TCS Digital 2020 - Count the number with unique digits in a range 
 import java.util.*;
+
 class One{
-    public static void main(String[] args){
-        Scanner scanner=new Scanner(System.in);
-        int count=0;
-        int m=scanner.nextInt();
-        int n=scanner.nextInt();
-        String num;
-        for(int i=m;i<=n;i++){
-            num=Integer.toString(i);
-            for(int j=0;j<num.length();j++){
-                for(int k=j+1;k<num.length();k++){
-                    if(num.charAt(j)==num.charAt(k)){
-                        count++;
-                        break;
-                    }
-                }
+
+    public static int CountNumbersHavingUniqueDigits(int min, int max) {
+        int count = 0;
+        for (int i = min; i <= max; i++) {
+            if (checkUniqueDigits(i)) {
+                count++;
             }
         }
-        System.out.println((n-m)-count);
+        return count;
+    }
+   
+    public static boolean checkUniqueDigits(int number) {
+        int[] appearingDigits = new int[10];
+        while (number > 0) {
+            int digit = number % 10;
+            appearingDigits[digit]++;
+            number /= 10;
+        }
+        for (int i = 0; i < 10; i++) {
+            if (appearingDigits[i] > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int start=scanner.nextInt();
+        int end=scanner.nextInt();
+        System.out.println(CountNumbersHavingUniqueDigits(start,end));
+        scanner.close();
     }
 }
